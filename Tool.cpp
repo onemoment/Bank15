ee015718b8db0ac610fa7f5b86133212b84c51d1
Krystian Szklarek <vix.mail@wp.pl>
2016-01-06 19:03:44 +0100

    visual studio

--- Tool.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <sstream>
-#include "Tool.h"
-
-std::string Tool::asString(int input){
-  std::ostringstream container ;
-  container << input ;
-  return container.str();
-}
--- Tool.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-class Tool{
-public:
-  static std::string asString(int input);
-};
